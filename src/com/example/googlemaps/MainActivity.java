8095d8652ee312f533941ab2bef073a0fae33565
=csgsajeewa <csgsajeewa@gmail.com>
2013-09-28 07:47:22 +0530

    2 nd commit, map is displayed, db is working

--- src/com/example/googlemaps/MainActivity.java
+++ src/com/example/googlemaps/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.googlemaps;
 
+
+
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends Activity{
         
@@ -9,10 +13,31 @@ public class MainActivity extends Activity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.main_page);
         
        
     }
+    
+    public void showMap(View view){
+		
+		Intent intent = new Intent(this, MapView.class);
+		startActivity(intent);
+		
+	}
+    
+     public void showMyLocation(View view){
+		
+		Intent intent = new Intent(this, UserLocation.class);
+		startActivity(intent);
+		
+	}
+     public void showMapDB(View view){
+ 		
+ 		Intent intent = new Intent(this, DBActivity.class);
+ 		startActivity(intent);
+ 		
+ 	}
+	
 
     
 }
